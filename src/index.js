00a2d0db9df72f7b22939d8835d0758fa7df5bcb
lucas <lucasmengue96@hotmail.com>
2021-10-31 15:43:29 -0300

    refatorando as rotas de vehicles e chamando o controller de cada um do mesmo

--- src/index.js
+++ src/index.js
@@ -23,90 +23,10 @@ app.get("/api/ping", (request, response) => {
 });
 
 /* endpoints vehicles */
-app.get("/api/vehicles", async (request, response) => {
-  const db = await openDatabase();
-  const vehicles = await db.all(`
-    SELECT * FROM vehicles
-  `);
-  db.close();
-
-  response.send(vehicles);
-});
-
-app.post("/api/vehicles", async (request, response) => {
-  const { model, label, type, owner, observation } = request.body;
-  const db = await openDatabase();
-  const data = await db.run(
-    `
-    INSERT INTO vehicles (model, label, type, owner, observation)
-    VALUES (?, ?, ?, ?, ?)
-  `,
-    [model, label, type, owner, observation]
-  );
-  db.close();
-  response.send({
-    id: data.lastID,
-    model,
-    label,
-    type,
-    owner,
-    observation,
-  });
-});
-
-app.put("/api/vehicles/:id", async (request, response) => {
-  const { model, label, type, owner, observation } = request.body;
-  const { id } = request.params;
-  const db = await openDatabase();
-  const vehicle = await db.get(
-    `
-    SELECT * FROM vehicles WHERE id = ?
-  `,
-    [id]
-  );
-  if (vehicle) {
-    const data = await db.run(
-      `
-      UPDATE vehicles
-      SET model = ?,
-          label = ?,
-          type = ?,
-          owner = ?,
-          observation = ?
-      WHERE id = ?
-    `,
-      [model, label, type, owner, observation, id]
-    );
-    db.close();
-    response.send({
-      id,
-      model,
-      label,
-      type,
-      owner,
-      observation,
-    });
-    return;
-  }
-  db.close();
-  response.send(vehicle || {});
-});
-
-app.delete("/api/vehicles/:id", async (request, response) => {
-  const { id } = request.params;
-  const db = await openDatabase();
-  const data = await db.run(
-    `
-    DELETE FROM vehicles WHERE id = ?
-  `,
-    [id]
-  );
-  db.close();
-  response.send({
-    id,
-    message: `Veículo [${id}] removido com sucesso`,
-  });
-});
+app.get("/api/vehicles", listVehicles);
+app.post("/api/vehicles", insertVehicles);
+app.put("/api/vehicles/:id", updateVehicles);
+app.delete("/api/vehicles/:id", removeVehicle);
 
 /* endpoints activities */
 app.post("/api/activities/checkin", async (request, response) => {
